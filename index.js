08640114c13199d0f9f48a76d59dac5432792a24
Dylan Tin <dylant711@gmail.com>
2024-08-26 23:18:03 -0400

    finished continued

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(activity) {
+    return `This Saturday, I want to ${activity ? activity : "roller-skate"}!`;
+  }
+
+function mondayWork(activity) {
+    return `This Monday, I will ${activity ? activity : "go to the office"}.`;
+}
+
+function wrapAdjective(flair = "*") {
+    return function(adjective = "special") {
+      return `You are ${flair}${adjective}${flair}!`;
+    };
+  }
